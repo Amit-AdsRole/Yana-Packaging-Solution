71db4004c3c70e0ed9c2a300e1a3f9bafc34c379
Amit Kumar <a.kumar@adsrole.in>
2026-01-21 01:40:22 +0530

    Testing of New Branch AMit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans ">
-    hgvhvsj
-    </div>
+Heo section is pending    </div>
   );
 }
